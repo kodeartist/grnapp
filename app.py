6947a923fa5519d41a5429d6ff1941e3075c8d23
Todo App Developer <developer@todoapp.com>
2025-12-16 11:17:55 +0000

    Page Templates

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "<h1>HELLO</h1>"
+    """
+    Redirects to the dashboard.
+    """
+    return redirect(url_for('dashboard'))
 
+@app.route("/dashboard")
+def dashboard():
+    """Displays the main dashboard."""
+    return render_template('dashboard.html')
+
+@app.route("/users")
+def users():
+    """Displays user management page."""
+    return render_template('users.html')
+
+
+@app.route("/customers")
+def customers():
+    """Displays user management page."""
+    return render_template('customers.html')
+
+
+@app.route("/logs")
+def logs():
+    """Displays user management page."""
+    return render_template('logs.html')
+
+
+@app.route("/settings")
+def settings():
+    """Displays user management page."""
+    return render_template('settings.html')
 
 if __name__ == "__main__":
     app.run()
